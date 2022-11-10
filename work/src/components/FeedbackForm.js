8bbc95fdd86988c80933931d7e0d31e6c563917f
BitupanThakuria <bitupanthakuria1996@gmail.com>
2022-11-10 20:53:17 +0530

    hmm

--- work/src/components/FeedbackForm.js
+++ work/src/components/FeedbackForm.js
@@ -1,8 +1,24 @@
 import React from 'react'
+import Card from './shared/Card'
+import {useState} from 'react'
 
 function FeedbackForm() {
+
+    const[text, setText] = useState('')
+
+    const handleTextChange = (e) => {
+        setText(e.target.value)
+    }
   return (
-    <div>FeedbackForm</div>
+    <Card>FeedbackForm
+        <div className='input-group'>
+            <input type='text'
+            onChange={handleTextChange} 
+            placeholder = 'What kind of work do you want to do?'
+            value = {text}/>
+            <button type= 'submit'>Lets do it</button>
+        </div>
+    </Card>
   )
 }
 
